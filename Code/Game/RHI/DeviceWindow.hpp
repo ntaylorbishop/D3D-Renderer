e2a171eec89aea76365374f6dca215e216d73e90
Taylor Bishop <ntaylorbishop@gmail.com>
2017-06-24 11:20:49 -0500

    Refactored device and window creation into DeviceWindow

---
+++ Code/Game/RHI/DeviceWindow.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "Engine/General/Core/EngineCommon.hpp"
+#include <windows.h>
+#include <d3d11_1.h>
+#include <directxcolors.h>
+
+class DeviceWindow {
+public:
+	static void Initialize(HINSTANCE applicationInstanceHandle, int nCmdShow);
+	static void Destroy();
+
+	static void ClearAndPresent();
+
+private:
+	//STRUCTORS
+	DeviceWindow(HINSTANCE applicationInstanceHandle, int nCmdShow);
+	~DeviceWindow();
+
+	//INIT DESTROY
+	void InitWindow(HINSTANCE applicationInstanceHandle, int nCmdShow);
+	void CreateDevice();
+	void CleanupDeviceAndWindow();
+
+	HINSTANCE               m_hInst					= nullptr;
+	HWND                    m_hWnd					= nullptr;
+	D3D_DRIVER_TYPE         m_driverType			= D3D_DRIVER_TYPE_NULL;
+	D3D_FEATURE_LEVEL       m_featureLevel			= D3D_FEATURE_LEVEL_11_0;
+	ID3D11Device*           m_pd3dDevice			= nullptr;
+	ID3D11Device1*          m_pd3dDevice1			= nullptr;
+	ID3D11DeviceContext*    m_pImmediateContext		= nullptr;
+	ID3D11DeviceContext1*   m_pImmediateContext1	= nullptr;
+	IDXGISwapChain*         m_pSwapChain			= nullptr;
+	IDXGISwapChain1*        m_pSwapChain1			= nullptr;
+	ID3D11RenderTargetView* m_pRenderTargetView		= nullptr;
+	ID3D11VertexShader*     m_pVertexShader			= nullptr;
+	ID3D11PixelShader*      m_pPixelShader			= nullptr;
+	ID3D11InputLayout*      m_pVertexLayout			= nullptr;
+	ID3D11Buffer*           m_pVertexBuffer			= nullptr;
+
+	static DeviceWindow* s_deviceWindow;
+};							
--- Code/Game/TheGame.cpp
+++ Code/Game/TheGame.cpp
@@ -1,24 +1,8 @@
 #include "Game/TheGame.hpp"
 #include "Game/TheApp.hpp"
-#include <d3d11_1.h>
-#include <directxcolors.h>
+#include "Game/RHI/DeviceWindow.hpp"
 
 TheGame* TheGame::s_theGame = nullptr;
-
-//--------------------------------------------------------------------------------------
-// Global Variables
-//--------------------------------------------------------------------------------------
-HINSTANCE               g_hInst = nullptr;
-HWND                    g_hWnd = nullptr;
-D3D_DRIVER_TYPE         g_driverType = D3D_DRIVER_TYPE_NULL;
-D3D_FEATURE_LEVEL       g_featureLevel = D3D_FEATURE_LEVEL_11_0;
-ID3D11Device*           g_pd3dDevice = nullptr;
-ID3D11Device1*          g_pd3dDevice1 = nullptr;
-ID3D11DeviceContext*    g_pImmediateContext = nullptr;
-ID3D11DeviceContext1*   g_pImmediateContext1 = nullptr;
-IDXGISwapChain*         g_pSwapChain = nullptr;
-IDXGISwapChain1*        g_pSwapChain1 = nullptr;
-ID3D11RenderTargetView* g_pRenderTargetView = nullptr;
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -60,187 +44,13 @@ STATIC TheGame* TheGame::Get() {
 //---------------------------------------------------------------------------------------------------------------------------
 TheGame::TheGame(HINSTANCE applicationInstanceHandle, int nCmdShow)
 {
-	//INIT WINDOW
-	WNDCLASSEX wcex;
-	wcex.cbSize = sizeof(WNDCLASSEX);
-	wcex.style = CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc = WindowsMessageHandlingProcedure;
-	wcex.cbClsExtra = 0;
-	wcex.cbWndExtra = 0;
-	wcex.hInstance = applicationInstanceHandle;
-	wcex.hIcon = NULL;
-	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
-	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-	wcex.lpszMenuName = nullptr;
-	wcex.lpszClassName = L"TutorialWindowClass";
-	wcex.hIconSm = NULL;
-	RegisterClassEx(&wcex);
-
-	// Create window
-	g_hInst = applicationInstanceHandle;
-	RECT rc = { 0, 0, 800, 600 };
-	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
-	g_hWnd = CreateWindow(L"TutorialWindowClass", L"Direct3D 11 Tutorial 1: Direct3D 11 Basics",
-		WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
-		CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, nullptr, nullptr, applicationInstanceHandle,
-		nullptr);
-	DWORD err = GetLastError();
-
-	ShowWindow(g_hWnd, nCmdShow);
-
-
-	//CREATE DEVICE
-	HRESULT hr = S_OK;
-
-	GetClientRect(g_hWnd, &rc);
-	UINT width = rc.right - rc.left;
-	UINT height = rc.bottom - rc.top;
-
-	UINT createDeviceFlags = 0;
-#ifdef _DEBUG
-	createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
-#endif
-
-	D3D_DRIVER_TYPE driverTypes[] =
-	{
-		D3D_DRIVER_TYPE_HARDWARE,
-		D3D_DRIVER_TYPE_WARP,
-		D3D_DRIVER_TYPE_REFERENCE,
-	};
-	UINT numDriverTypes = ARRAYSIZE(driverTypes);
-
-	D3D_FEATURE_LEVEL featureLevels[] =
-	{
-		D3D_FEATURE_LEVEL_11_1,
-		D3D_FEATURE_LEVEL_11_0,
-		D3D_FEATURE_LEVEL_10_1,
-		D3D_FEATURE_LEVEL_10_0,
-	};
-	UINT numFeatureLevels = ARRAYSIZE(featureLevels);
-
-	for (UINT driverTypeIndex = 0; driverTypeIndex < numDriverTypes; driverTypeIndex++)
-	{
-		g_driverType = driverTypes[driverTypeIndex];
-		hr = D3D11CreateDevice(nullptr, g_driverType, nullptr, createDeviceFlags, featureLevels, numFeatureLevels,
-			D3D11_SDK_VERSION, &g_pd3dDevice, &g_featureLevel, &g_pImmediateContext);
-
-		if (hr == E_INVALIDARG)
-		{
-			// DirectX 11.0 platforms will not recognize D3D_FEATURE_LEVEL_11_1 so we need to retry without it
-			hr = D3D11CreateDevice(nullptr, g_driverType, nullptr, createDeviceFlags, &featureLevels[1], numFeatureLevels - 1,
-				D3D11_SDK_VERSION, &g_pd3dDevice, &g_featureLevel, &g_pImmediateContext);
-		}
-
-		if (SUCCEEDED(hr))
-			break;
-	}
-
-	// Obtain DXGI factory from device (since we used nullptr for pAdapter above)
-	IDXGIFactory1* dxgiFactory = nullptr;
-	{
-		IDXGIDevice* dxgiDevice = nullptr;
-		hr = g_pd3dDevice->QueryInterface(__uuidof(IDXGIDevice), reinterpret_cast<void**>(&dxgiDevice));
-		if (SUCCEEDED(hr))
-		{
-			IDXGIAdapter* adapter = nullptr;
-			hr = dxgiDevice->GetAdapter(&adapter);
-			if (SUCCEEDED(hr))
-			{
-				hr = adapter->GetParent(__uuidof(IDXGIFactory1), reinterpret_cast<void**>(&dxgiFactory));
-				adapter->Release();
-			}
-			dxgiDevice->Release();
-		}
-	}
-
-	// Create swap chain
-	IDXGIFactory2* dxgiFactory2 = nullptr;
-	hr = dxgiFactory->QueryInterface(__uuidof(IDXGIFactory2), reinterpret_cast<void**>(&dxgiFactory2));
-	if (dxgiFactory2)
-	{
-		// DirectX 11.1 or later
-		hr = g_pd3dDevice->QueryInterface(__uuidof(ID3D11Device1), reinterpret_cast<void**>(&g_pd3dDevice1));
-		if (SUCCEEDED(hr))
-		{
-			(void)g_pImmediateContext->QueryInterface(__uuidof(ID3D11DeviceContext1), reinterpret_cast<void**>(&g_pImmediateContext1));
-		}
-
-		DXGI_SWAP_CHAIN_DESC1 sd;
-		ZeroMemory(&sd, sizeof(sd));
-		sd.Width = width;
-		sd.Height = height;
-		sd.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-		sd.SampleDesc.Count = 1;
-		sd.SampleDesc.Quality = 0;
-		sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-		sd.BufferCount = 1;
-
-		hr = dxgiFactory2->CreateSwapChainForHwnd(g_pd3dDevice, g_hWnd, &sd, nullptr, nullptr, &g_pSwapChain1);
-		if (SUCCEEDED(hr))
-		{
-			hr = g_pSwapChain1->QueryInterface(__uuidof(IDXGISwapChain), reinterpret_cast<void**>(&g_pSwapChain));
-		}
-
-		dxgiFactory2->Release();
-	}
-	else
-	{
-		// DirectX 11.0 systems
-		DXGI_SWAP_CHAIN_DESC sd;
-		ZeroMemory(&sd, sizeof(sd));
-		sd.BufferCount = 1;
-		sd.BufferDesc.Width = width;
-		sd.BufferDesc.Height = height;
-		sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-		sd.BufferDesc.RefreshRate.Numerator = 60;
-		sd.BufferDesc.RefreshRate.Denominator = 1;
-		sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-		sd.OutputWindow = g_hWnd;
-		sd.SampleDesc.Count = 1;
-		sd.SampleDesc.Quality = 0;
-		sd.Windowed = TRUE;
-
-		hr = dxgiFactory->CreateSwapChain(g_pd3dDevice, &sd, &g_pSwapChain);
-	}
-
-	// Note this tutorial doesn't handle full-screen swapchains so we block the ALT+ENTER shortcut
-	dxgiFactory->MakeWindowAssociation(g_hWnd, DXGI_MWA_NO_ALT_ENTER);
-
-	dxgiFactory->Release();
-
-
-	// Create a render target view
-	ID3D11Texture2D* pBackBuffer = nullptr;
-	hr = g_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), reinterpret_cast<void**>(&pBackBuffer));
-
-	hr = g_pd3dDevice->CreateRenderTargetView(pBackBuffer, nullptr, &g_pRenderTargetView);
-	pBackBuffer->Release();
-
-	g_pImmediateContext->OMSetRenderTargets(1, &g_pRenderTargetView, nullptr);
-
-	// Setup the viewport
-	D3D11_VIEWPORT vp;
-	vp.Width = (FLOAT)width;
-	vp.Height = (FLOAT)height;
-	vp.MinDepth = 0.0f;
-	vp.MaxDepth = 1.0f;
-	vp.TopLeftX = 0;
-	vp.TopLeftY = 0;
-	g_pImmediateContext->RSSetViewports(1, &vp);
+	DeviceWindow::Initialize(applicationInstanceHandle, nCmdShow);
 }
 
 
 //---------------------------------------------------------------------------------------------------------------------------
 TheGame::~TheGame() {
-	if (g_pImmediateContext) g_pImmediateContext->ClearState();
-
-	if (g_pRenderTargetView) g_pRenderTargetView->Release();
-	if (g_pSwapChain1) g_pSwapChain1->Release();
-	if (g_pSwapChain) g_pSwapChain->Release();
-	if (g_pImmediateContext1) g_pImmediateContext1->Release();
-	if (g_pImmediateContext) g_pImmediateContext->Release();
-	if (g_pd3dDevice1) g_pd3dDevice1->Release();
-	if (g_pd3dDevice) g_pd3dDevice->Release();
+	DeviceWindow::Destroy();
 }
 
 
@@ -265,9 +75,7 @@ void TheGame::Update(float deltaSeconds) {
 
 //---------------------------------------------------------------------------------------------------------------------------
 STATIC void TheGame::Render() {
-	// Just clear the backbuffer
-	float col[] = {1.f, 0.f, 0.f, 1.f};
-	g_pImmediateContext->ClearRenderTargetView(g_pRenderTargetView, col);
-	g_pSwapChain->Present(0, 0);
+
+	DeviceWindow::ClearAndPresent();
 }
 
